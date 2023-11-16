fe333982a15f24ac7910e9ac6b5621b8ae49f3ee
J Hope <55669583+badmintonstem@users.noreply.github.com>
2023-11-16 15:16:42 +0000

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,11 +4,11 @@
 # Subprograms
 # -------------------------
 def c_to_f(centigrade):
-
+  return (centigrade * 1.8) + 32
 
 # -------------------------
 # Main program
 # -------------------------
-centigrade =
-fahrenheit = 
+centigrade = -5
+fahrenheit = c_to_f(centigrade)
 print(centigrade, "degrees Centigrade is", fahrenheit, "degrees Fahrenheit.")
